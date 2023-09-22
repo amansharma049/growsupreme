cfd0d1b1508f48c288e36ebf41fb673e29a7ab67
Aman Sharma <98725523+amansharma049@users.noreply.github.com>
2023-09-22 14:46:09 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function nav(){
+    document.getElementById("navlinks").style.display="block";
+    document.getElementById("nav-bar").style.display="none";
+    document.getElementById("nav-barclose").style.display="block";
+}
+function navclose(){
+    document.getElementById("navlinks").style.display="none";
+    document.getElementById("nav-bar").style.display="block";
+    document.getElementById("nav-barclose").style.display="none";
+}
